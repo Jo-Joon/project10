f5b71ce11e9fb98f0546beaa68cae616aeb0aa7f
91hongppie <91hongppie@gmail.com>
2019-11-21 10:33:17 +0900

    feature/review

--- movies/urls.py
+++ movies/urls.py
@@ -5,6 +5,9 @@ app_name='movies'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:movie_pk>/', views.detail, name='detail'),
+    path('<int:movie_pk>/review_create', views.review_create, name='review_create'),
     path('<int:movie_pk>/like/', views.like, name='like'),
+    path('<int:movie_pk>/follow/<int:user_pk>/', views.follow, name='follow'),
+
 
 ]
