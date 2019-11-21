31f6134d84c02b4a595cf7bf8ec737e8ef3643f0
Gagle637 <gagle637@gmail.com>
2019-11-21 10:31:11 +0900

    feature/nav&profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,7 @@ app_name='accounts'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:user_pk>/', views.user_profile, name='profile'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
