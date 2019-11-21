b44f602213a364665065c2ea280cacc45369a491
91hongppie <91hongppie@gmail.com>
2019-11-21 12:14:34 +0900

    feature/reviewdelete

--- movies/urls.py
+++ movies/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:movie_pk>/', views.detail, name='detail'),
     path('<int:movie_pk>/review_create', views.review_create, name='review_create'),
+    path('<int:movie_pk>/review_delete/<int:review_pk>', views.review_delete, name='review_delete'),
     path('<int:movie_pk>/like/', views.like, name='like'),
-    path('<int:movie_pk>/follow/<int:user_pk>/', views.follow, name='follow'),
+    path('<int:movie_pk>/follow/<int:person_pk>/', views.follow, name='follow'),
 
 
 ]
